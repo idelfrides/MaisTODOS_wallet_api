567561c7c7ae53adbeced2cc6d6aba53029b306a
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-11 17:49:57 -0300

    add authtoken endpoint on urls to make authenticaton.py

--- redeVaregista/api/viewsets.py
+++ redeVaregista/api/viewsets.py
@@ -2,10 +2,13 @@ import imp
 from rest_framework.viewsets import ModelViewSet
 from .serializers import RedeVaregistaSerializer
 from redeVaregista.models import RedeVaregista
-# from rest_framework.permissions import IsAuthenticated
-# from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 class RedeVaregistaViewSet(ModelViewSet):
 
     queryset = RedeVaregista.objects.all()
     serializer_class = RedeVaregistaSerializer
+
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (TokenAuthentication, )
