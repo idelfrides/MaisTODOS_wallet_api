bd84b57b0bffee61d928da479484100d5c4dd668
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-11 17:41:30 -0300

    add authtoken endpoint to urls.py

--- wallet_api/urls.py
+++ wallet_api/urls.py
@@ -13,18 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
 from rest_framework import routers
 from redeVaregista.api.viewsets import RedeVaregistaViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 
 router.register(
-    'redevaregista', RedeVaregistaViewSet, base_name='RedeVaregista')  
+    'redevaregista', RedeVaregistaViewSet, base_name='RedeVaregista')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token)
 ]
