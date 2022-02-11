445f0f16effa6781b16190b9cc6a5f6f5be5959d
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-11 17:07:52 -0300

    install rest_framework on apps and configure router to access the wallet API

--- wallet_api/urls.py
+++ wallet_api/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
+from rest_framework import routers
+from redeVaregista.api.viewsets import RedeVaregistaViewSet
+
+router = routers.DefaultRouter()
+
+router.register(
+    'redevaregista', RedeVaregistaViewSet, base_name='RedeVaregista')  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
