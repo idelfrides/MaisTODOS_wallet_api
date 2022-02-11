0074caa5da517f0613cb0f75bb37146af598528b
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-11 20:23:13 -0300

    configured search cpacity

--- redeVaregista/api/viewsets.py
+++ redeVaregista/api/viewsets.py
@@ -1,7 +1,8 @@
-import imp
+from re import search
 from rest_framework.viewsets import ModelViewSet
 from .serializers import RedeVaregistaSerializer
 from redeVaregista.models import RedeVaregista
+from rest_framework.filters import SearchFilter
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -9,6 +10,9 @@ class RedeVaregistaViewSet(ModelViewSet):
 
     queryset = RedeVaregista.objects.all()
     serializer_class = RedeVaregistaSerializer
+
+    filter_backends = (SearchFilter, )
+    search_fields = ('name', 'sold_at')
 
     permission_classes = (IsAuthenticated, )
     authentication_classes = (TokenAuthentication, )
