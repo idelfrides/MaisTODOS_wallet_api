0a010fd097bcd243f56ef7dd8a4fdc6893f4fc9f
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-11 20:21:35 -0300

    I left a TODO

--- redeVaregista/models.py
+++ redeVaregista/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from customer.models import Customer
 
 class RedeVaregista(models.Model):
+
     name = models.CharField(verbose_name='Nome do varegista', max_length=300)
     total = models.IntegerField(verbose_name='Total de registros')
     sold_at = models.DateTimeField(
         verbose_name='Data de venda', null=False)
 
-    # customer = models.ManyToManyField()
+    customer = models.ManyToManyField(Customer)
+    # TODO: make all content of a customer to appear  when call get  redevaregista
+
     # product = models.ManyToManyField()
 
     def __str__(self):
