b59d0a0f1be7a3b75980c02e16d75d246a2f423f
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-12 13:42:03 -0300

    finish viewset.py configuration for endpoint product

--- products/api/viewsets.py
+++ products/api/viewsets.py
@@ -1,7 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
+from products.api.serializers import ProductSerializer
 from products.models import Products
 
 class ProductViewSet(ModelViewSet):
 
     queryset = Products.objects.all()
-    # serializer_class =
+    serializer_class = ProductSerializer
