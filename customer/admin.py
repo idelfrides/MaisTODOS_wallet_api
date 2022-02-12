13382d42cdcd39ed48739583ab05c91b46a90dab
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-12 12:51:48 -0300

    customize admin for customer

--- customer/admin.py
+++ customer/admin.py
@@ -3,4 +3,16 @@ from django.contrib import admin
 from .models import Customer
 # Register your models here.
 
-admin.site.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'cpf']
+    list_display_links = ['name']
+    list_filter = ['name']
+    list_editable = ['cpf']
+    class Meta:
+        model = Customer
+
+
+admin.site.register(
+    Customer,
+    CustomerModelAdmin
+)
