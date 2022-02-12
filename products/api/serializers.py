d55de2cb96858b7e85f1213e8071ec0a6449d888
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-12 17:06:32 -0300

    make  id  visible on response

--- products/api/serializers.py
+++ products/api/serializers.py
@@ -1,5 +1,3 @@
-from dataclasses import fields
-from django.urls import clear_script_prefix
 from rest_framework.serializers import ModelSerializer
 from products.models import Products
 
@@ -7,4 +5,4 @@ class ProductSerializer(ModelSerializer):
 
     class Meta:
         model = Products
-        fields = ['product_type', 'value', 'quantity']
+        fields = ['id', 'product_type', 'value', 'quantity']
