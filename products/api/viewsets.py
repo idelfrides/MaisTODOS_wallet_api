0184bafdc8d2fc6f521c91d65064a608f06cf9e9
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-12 17:04:57 -0300

    add authentication and permission to access product via token

--- products/api/viewsets.py
+++ products/api/viewsets.py
@@ -1,8 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
 from products.api.serializers import ProductSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from products.models import Products
 
 class ProductViewSet(ModelViewSet):
 
     queryset = Products.objects.all()
     serializer_class = ProductSerializer
+
+    # filter_backends = (SearchFilter, )
+    # search_fields = ('name', 'sold_at')
+
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (TokenAuthentication, )
