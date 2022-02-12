aceda8d45c689df37fb7182393796661eaf0c391
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-12 11:07:06 -0300

    change attribute order

--- redeVaregista/models.py
+++ redeVaregista/models.py
@@ -3,10 +3,17 @@ from customer.models import Customer
 
 class RedeVaregista(models.Model):
 
-    name = models.CharField(verbose_name='Nome do varegista', max_length=300)
-    total = models.IntegerField(verbose_name='Total de registros')
+    name = models.CharField(
+        verbose_name='Nome do varegista',
+        max_length=300
+    )
+    total = models.IntegerField(
+        verbose_name='Total de registros'
+    )
     sold_at = models.DateTimeField(
-        verbose_name='Data de venda', null=False)
+        verbose_name='Data de venda',
+        null=False
+    )
 
     customer = models.ManyToManyField(Customer)
     # TODO: make all content of a customer to appear  when call get  redevaregista
