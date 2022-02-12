3ba5813c19a1967936688a6e85883da7709f7c0b
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-12 17:22:54 -0300

    add searching capacity

--- products/api/viewsets.py
+++ products/api/viewsets.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from products.api.serializers import ProductSerializer
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter
 from products.models import Products
 
 class ProductViewSet(ModelViewSet):
@@ -9,8 +10,8 @@ class ProductViewSet(ModelViewSet):
     queryset = Products.objects.all()
     serializer_class = ProductSerializer
 
-    # filter_backends = (SearchFilter, )
-    # search_fields = ('name', 'sold_at')
+    filter_backends = (SearchFilter, )
+    search_fields = ('name', 'value', 'quantity')
 
     permission_classes = (IsAuthenticated, )
     authentication_classes = (TokenAuthentication, )
