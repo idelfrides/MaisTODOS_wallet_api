7e3f8e00d13f0d1085254a8889e9329510a91029
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-12 12:50:07 -0300

    finish viewset and add product attribute to redevaregista class model

--- redeVaregista/models.py
+++ redeVaregista/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from customer.models import Customer
-# from products.models import Products
+from products.models import Products
 
 class RedeVaregista(models.Model):
 
@@ -8,18 +8,18 @@ class RedeVaregista(models.Model):
         verbose_name='Nome do varegista',
         max_length=300
     )
-    total = models.IntegerField(
+    total =models.IntegerField(
         verbose_name='Total de registros'
     )
     sold_at = models.DateTimeField(
         verbose_name='Data de venda',
         null=False
     )
+    customer = models.ManyToManyField(Customer)
+    product = models.ManyToManyField(Products)
 
-    customer = models.ForeignKey(Customer)
     # TODO: make all content of a customer to appear  when call get  redevaregista
 
-    # product = models.ManyToManyField(Products)
 
     def __str__(self):
         return self.name
