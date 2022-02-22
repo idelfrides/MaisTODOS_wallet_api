ef0dc52b5a4be3e707fb5571dff6ad2722c9b0d1
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-22 19:36:37 -0300

    clean model for customer

--- customer/models.py
+++ customer/models.py
@@ -1,16 +1,15 @@
+from enum import unique
 from os import name
 from django.db import models
 from cpf_field.models import CPFField
-# Create your models here.
 
 
 class Customer(models.Model):
 
     cpf = CPFField('CPF do cliente')
-
     name = models.CharField(
         verbose_name='Nome do cliente',
-        max_length=300
+        max_length=300,
     )
 
     def __str__(self):
