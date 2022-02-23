49e5cdbb2d25b235b5be8b38198c4bfc1535445e
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-23 08:16:51 -0300

    1:move calculate discount method from viewset to lib. 2: move make_request method to views

--- redeVaregista/api/viewsets.py
+++ redeVaregista/api/viewsets.py
@@ -9,15 +9,12 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from utils_folder.lib_manager_api import (
-    customer_validation
+    customer_validation,
+    product_validation,
+    calculate_discount
 )
-import requests
+from views import make_request
 
-
-DISCOUNT = {
-    'DIAMANTE': 5,
-    'OURO': 3
-}
 
 
 class RedeVaregistaViewSet(ModelViewSet):
@@ -33,17 +30,20 @@ class RedeVaregistaViewSet(ModelViewSet):
 
     @action(methods=['POST', 'OPTIONS'], detail=False)
     def cashback(self, request, *args, **kwarg):
+
         content_to_request = {}
+
         cpf = request.data.get('customer', {})['document']
 
         customer_id = customer_validation(cpf)
+
         if str(customer_id).isnumeric():
             request.data['customer']  = customer_id
         else:
             return  Response({'Response': customer_id})
 
         result_code, product_list, type_value_product_list = (
-            self.product_validation(request.data)
+            product_validation(request.data)
         )
 
         if result_code == 404:
@@ -54,85 +54,11 @@ class RedeVaregistaViewSet(ModelViewSet):
 
         result_data = super().create(request, args, kwarg)
 
-        discount_result = self.calculate_discount(type_value_product_list)
+        discount_result = calculate_discount(type_value_product_list)
 
         content_to_request['cpf'] = cpf
         content_to_request['cashback'] = discount_result
 
-        cashback_result = self.make_request(content_to_request)
+        cashback_result = make_request(content_to_request)
 
         return Response(cashback_result[1])
-
-
-    def customer_validation(self, cpf):
-        customer = Customer.objects.get(cpf=cpf)
-
-        if not customer:
-            result_ = 'INFORMED CPF DO NOT EXISTS.'
-        else:
-            result_ = customer.id
-
-        return result_
-
-
-    def product_validation(self, request_data):
-
-        products_list = []
-        type_value_product_list = []
-        type_value_dict = {}
-        result_code = 200
-
-        all_product_code = request_data.get('product')
-
-        for prod_dict in all_product_code:
-            product_code = prod_dict.get('productCode', {})
-
-            product = Products.objects.get(productCode=product_code)
-
-            if product:
-                products_list.append(product.id)
-                type_value_dict[str(product.product_type)] = product.value
-                type_value_product_list.append(type_value_dict)
-            else:
-                result_ = f'PRODUCT CODE {product_code} DO NOT EXISTS.'
-                products_list.append(result_)
-                result_code = 404
-                break
-
-        return result_code, products_list, type_value_product_list
-
-
-    def calculate_discount(self, type_value_product_list):
-        total_discount_value = 0
-
-        for product in type_value_product_list:
-            for key, prod_value in product.items():
-
-                discount = DISCOUNT.get(key, 0)
-                discount_value  =  (float(prod_value) * (discount/100))
-
-                total_discount_value += discount_value
-
-        return total_discount_value
-
-
-    def make_request(self, request_data):
-        url = 'https://5efb30ac80d8170016f7613d.mockapi.io/api/mock/Cashback'
-
-        data_content = {
-            'document': request_data['cpf'],
-            'cashback': request_data['cashback']
-        }
-
-        try:
-            payload = requests.post(url, json=data_content)
-        except Exception as err:
-            print(f'EXCEPTION: {err}')
-            payload = requests.post(url, data=data_content)
-
-        if payload.status_code in (200, 201):
-            content_payload = payload.json()
-        else:
-            content_payload = {}
-
-        return payload.status_code, content_payload
--- redeVaregista/views.py
+++ redeVaregista/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+
+
+def make_request(self, request_data):
+    url = 'https://5efb30ac80d8170016f7613d.mockapi.io/api/mock/Cashback'
+
+    data_content = {
+        'document': request_data['cpf'],
+        'cashback': request_data['cashback']
+    }
+
+    try:
+        payload = requests.post(url, json=data_content)
+    except Exception as err:
+        print(f'EXCEPTION: {err}')
+        payload = requests.post(url, data=data_content)
+
+    if payload.status_code in (200, 201):
+        content_payload = payload.json()
+    else:
+        content_payload = {}
+
+    return payload.status_code, content_payload
