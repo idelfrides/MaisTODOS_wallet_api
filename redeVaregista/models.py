14c656d50cd3c9cfc0a4e700f385586116156fd5
Idelfrides Jorge <idelfridesjorgepapai@gmail.com>
2022-03-29 16:52:10 -0300

    Update models.py

--- redeVaregista/models.py
+++ redeVaregista/models.py
@@ -26,24 +26,7 @@ class RedeVaregista(models.Model):
     product = models.ManyToManyField(
         Products
     )
-
-
-
-    # TODO: make all content of a customer to appear  when call get  redevaregista
+    
 
     def __str__(self):
         return self.name
-
-'''
-class CreatedUUID(models.Model):
-
-    id = models.UUIDField(
-        verbose_name='Codigo Unico',
-        primary_key=True,
-        default=uuid.uuid4,
-        editable=False
-    )
-
-    code_help = id.split('-')
-    code = code_help[0][:2] + code_help[1][:2] + code_help[2][:2] + code_help[3][:2] + code_help[4][:2]
-'''
